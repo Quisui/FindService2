25c66f638b8a1bf2ce4e1cb101407dba126d18d9
Hugo Lucas <daniellucas10522@gmail.com>
2018-10-24 23:20:44 -0600

    Registro de usuarios

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -12,73 +12,83 @@
           </ul>
         </div>
         @endif
-
-        {!!Form::open(array('url'=>'prueba','method'=>'POST','autocomplete'=>'off'))!!}
-        {{Form::token()}}
-        <!--Nombre y Apellido-->
-        <div class="row">
-          <div class="input-field col s12 l6">
-            <input id="sign_up_name" name="nombre" type="text" class="validate">
-            <label for="sign_up_name">Nombre</label>
+        <div class="col s6 l7">
+          {!!Form::open(array('url'=>'prueba','method'=>'POST','autocomplete'=>'off'))!!}
+          {{Form::token()}}
+          <!--Nombre y Apellido-->
+          <div class="row">
+            <div class="input-field col s12 l6">
+              <<<<<<< HEAD
+              <input id="sign_up_name" name="nombre" type="text" class="validate" required>
+              <label for="sign_up_name">Nombre</label>
+            </div>
+            <div class="input-field col s12 l6">
+              <input id="sign_up_last_name" name="apellido" type="text" class="validate" required>
+              >>>>>>> Mariano
+              <label for="sign_up_last_name">Apellido</label>
+            </div>
           </div>
-          <div class="input-field col s12 l6">
-            <input id="sign_up_last_name" name="apellido" type="text" class="validate">
-            <label for="sign_up_last_name">Apellido</label>
+          <!--Nombre de Usuario-->
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix">person</i>
+              <input id="sign_up_nickname" name="nombre_usuario" type="text" class="validate">
+              <label for="sign_up_nickname">Nombre de Usuario</label>
+            </div>
           </div>
-        </div>
-        <!--Nombre de Usuario-->
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons prefix">person</i>
-            <input id="sign_up_nickname" name="nombre_usuario" type="text" class="validate">
-            <label for="sign_up_nickname">Nombre de Usuario</label>
+          <!--Correo-->
+          <div class="row">
+            <div class="input-field col s12 m7">
+              <i class="material-icons prefix">alternate_email</i>
+              <<<<<<< HEAD
+              <input id="sign_up_email" name="correo" type="text" class="validate" required>
+              =======
+              >>>>>>> Mariano
+              <label for="sign_up_email">Correo</label>
+            </div>
+            <div class="input-field col s12 m5">
+              <i class="material-icons prefix">phone</i>
+              <input id="sign_up_phone" name="numero_telefono" type="text" class="validate">
+              <label for="sign_up_phone">Numero de Teléfono</label>
+            </div>
           </div>
-        </div>
-        <!--Correo-->
-        <div class="row">
-          <div class="input-field col s12 m7">
-            <i class="material-icons prefix">alternate_email</i>
-            <input id="sign_up_email" name="correo" type="text" class="validate">
-            <label for="sign_up_email">Correo</label>
+          <!--Contraseña y confirmacion-->
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix">vpn_key</i>
+              <<<<<<< HEAD
+              <input id="sign_up_password" name="contraseña" type="password" class="validate" required>
+              =======
+              >>>>>>> Mariano
+              <label for="sign_up_password">Contraseña</label>
+            </div>
           </div>
-          <div class="input-field col s12 m5">
-            <i class="material-icons prefix">phone</i>
-            <input id="sign_up_phone" name="numero_telefono" type="text" class="validate">
-            <label for="sign_up_phone">Numero de Teléfono</label>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix">vpn_key</i>
+              <input id="sign_up_confirm_password" type="password" class="validate" required>
+              <label for="sign_up_confirm_password">Confirma tu Contraseña</label>
+            </div>
           </div>
-        </div>
-        <!--Contraseña y confirmacion-->
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons prefix">vpn_key</i>
-            <input id="sign_up_password" name="contraseña" type="password" class="validate">
-            <label for="sign_up_password">Contraseña</label>
-          </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons prefix">vpn_key</i>
-            <input id="sign_up_confirm_password" type="password" class="validate">
-            <label for="sign_up_confirm_password">Confirma tu Contraseña</label>
-          </div>
-        </div>
-        <!--Fecha de nacimiento-->
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons prefix">date_range</i>
-            <input id="sign_up_birthday" name="fecha_nacimiento" type="text" class="datepicker">
-            <label for="sign_up_birthday">Fecha de Nacimiento</label>
-          </div>
-          <script>
-            document.addEventListener('DOMContentLoaded', function() {
-              var elems = document.querySelectorAll('.datepicker');
-              var instances = M.Datepicker.init(elems, {
-                autoClose: true,
-                format: 'yyyy-mm-dd',
-                yearRange: 50
+          <!--Fecha de nacimiento-->
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix">date_range</i>
+              <input id="sign_up_birthday" name="fecha_nacimiento" type="text" class="datepicker">
+              <label for="sign_up_birthday">Fecha de Nacimiento</label>
+            </div>
+            <script>
+              document.addEventListener('DOMContentLoaded', function() {
+                var elems = document.querySelectorAll('.datepicker');
+                var instances = M.Datepicker.init(elems, {
+                  autoClose: true,
+                  format: 'yyyy-mm-dd',
+                  yearRange: 50
+                });
               });
-            });
-          </script>
+            </script>
+          </div>
+
         </div>
         <!--Boton de registro-->
         <div class="row">
@@ -97,7 +107,7 @@
             <span class="card-title">También puedes registrarte usando:</span>
           </div>
           <div class="card-action center">
-            <a href="{{url('/auth/google')}}" class="btn-flat waves-effect waves-red"><img class="left" src="imagenes/Google.png" alt="Google" height="30px">Google</a>
+            <a href="#" class="btn-flat waves-effect waves-red"><img class="left" src="imagenes/Google.png" alt="Google" height="30px">Google</a>
             <a href="#" class="btn-flat waves-effect waves-red"><img class="left" src="imagenes/Facebook.png" alt="Facebook" height="30px">Facebook</a>
           </div>
         </div>
